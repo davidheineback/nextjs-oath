3e4d170e33dd235d6e1c269ef833a75f9e70ce18
Tamim Jabr <tj222kg@student.lnu.se>
2022-03-21 19:31:17 +0100

    show consent page when clicking on login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+import Link from 'next/link';
 import React from 'react';
+import getGoogleOAuthUrl from '../utils/getGoogleUrl';
 
-function Home() {
+function Home () {
   return (
     <div>
       <h1>NEXT JS</h1>
+      <Link href={getGoogleOAuthUrl()} passHref>
+        <button>
+          Login with Google
+        </button>
+      </Link>
     </div>
   )
 }
